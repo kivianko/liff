1566b924c0d20845b5cf0ff46007a9683fd3c115
kivianko <kivianko@gmail.com>
2022-03-30 11:35:07 +0900

    build: deployテスト

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  // 本番時はgithubリポジトリをルートパスにする
+  base: process.env.NODE_ENV === "production" ? "https://kivianko.github.io/liff/" : "./",
   build: {
     outDir: "docs",
   },
