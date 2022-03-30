03b38bcddeaed4280210fae85c00a80eee5cd971
kivianko <kivianko@gmail.com>
2022-03-30 10:50:06 +0900

    build: GithubPagesにデプロイ準備

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import "./index.css";
 import router from "./router"; // router.tsをインポート
 
 // VueRouter が動き始める前にLIFFを初期化する
-await liff.init({ liffId: import.meta.env.VITE_LIFF_ID });
+liff.init({ liffId: import.meta.env.VITE_LIFF_ID });
 
 const app = createApp(App);
 app.use(router); // Vue Routerを使用する
