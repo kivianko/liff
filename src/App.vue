050a1851889edbfbea7a22bec88c9233f158b174
kivianko <kivianko@gmail.com>
2022-03-30 12:01:23 +0900

    build: deployテスト

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+    <h1>テスト</h1>
+  </div>
   <!-- パスとマッチしたページが表示される -->
-  <router-view></router-view>
+  <router-view />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   // 本番時はgithubリポジトリをルートパスにする
-  base: process.env.NODE_ENV === "production" ? "https://kivianko.github.io/liff/" : "./",
+  base: "./",
   build: {
     outDir: "docs",
   },
