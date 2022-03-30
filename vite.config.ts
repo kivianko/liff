19502645368224fa75bf9fafd4621f0093ec2e9c
kivianko <kivianko@gmail.com>
2022-03-30 12:48:11 +0900

    build: deployテスト

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import * as path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -5,6 +6,12 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   // 本番時はgithubリポジトリをルートパスにする
   base: process.env.NODE_ENV === "production" ? "/liff/" : "./",
+  resolve: {
+    // @/ によるimportをできるようにする。
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+    },
+  },
   build: {
     outDir: "docs",
   },
